42fc66304c801f4ba35cd02ca97194b357cc48fa
Ayoyemi Ajisebutu <a.ajisebutu@primebytes.ca>
2024-09-10 11:19:37 -0400

    Initial Commit

--- playlist.py
+++ playlist.py
@@ -2,9 +2,11 @@
 from pytubefix import Playlist
 from pytubefix.cli import on_progress
  
-url = "url"
+url = "https://www.youtube.com/watch?v=jqCo89XFEoc&list=OLAK5uy_mo8OCMdAkRJtBRYgupsCD71RWsmNIJTUU"
+url = "https://www.youtube.com/watch?v=qSIOp_K5GMw&list=RDQMbO4w_MU_6r8&index=2"
 
 pl = Playlist(url)
+print("Downloading...") 
 
 for video in pl.videos:
     ys = video.streams.get_audio_only()
